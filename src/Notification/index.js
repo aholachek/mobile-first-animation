cb0287d672292cf3b97785de9246efb7ab5450bc
Kunuk Nykjær <kunukn@gmail.com>
2019-11-04 12:51:13 +0100

    remove warnings

--- src/Notification/index.js
+++ src/Notification/index.js
@@ -1,65 +1,65 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
-import { animated } from "react-spring"
-import { useDrag } from "react-use-gesture"
-import useVelocityTrackedSpring from "../useVelocityTrackedSpring"
+import { animated } from "react-spring";
+import { useDrag } from "react-use-gesture";
+import useVelocityTrackedSpring from "../useVelocityTrackedSpring";
 
 import {
   StyledNotification,
   StyledNotificationContainer,
   StyledContainer
-} from "./styled-components"
+} from "./styled-components";
 import {
   findNearestNumberInArray,
   projection,
   rubberBandIfOutOfBounds
-} from "../utilities"
+} from "../utilities";
 
-const yStops = [0, 120]
-const threshold = 10
+const yStops = [0, 120];
+const threshold = 10;
 
 const Notification = ({ children, hideNotification }) => {
-  const [{ y }, set] = useVelocityTrackedSpring(() => ({ y: yStops[1] }))
+  const [{ y }, set] = useVelocityTrackedSpring(() => ({ y: yStops[1] }));
 
   useEffect(() => {
-    set({ y: 0 })
-  }, [set])
+    set({ y: 0 });
+  }, [set]);
 
   const bind = useDrag(
     ({ last, movement: [, movementY], vxvy: [, velocityY], memo }) => {
       if (!memo) {
-        const isIntentionalGesture = Math.abs(movementY) > threshold
-        if (!isIntentionalGesture) return
-        memo = y.value - movementY
+        const isIntentionalGesture = Math.abs(movementY) > threshold;
+        if (!isIntentionalGesture) return;
+        memo = y.value - movementY;
       }
 
       if (last) {
-        const projectedEndpoint = y.value + projection(velocityY)
-        const point = findNearestNumberInArray(projectedEndpoint, yStops)
+        const projectedEndpoint = y.value + projection(velocityY);
+        const point = findNearestNumberInArray(projectedEndpoint, yStops);
 
-        const notificationClosed = point === yStops[1]
+        const notificationClosed = point === yStops[1];
 
         return set({
           y: notificationClosed ? yStops[1] : yStops[0],
           onRest: notificationClosed ? hideNotification : () => {},
           immediate: false
-        })
+        });
       }
 
       const newY = rubberBandIfOutOfBounds(
         yStops[0],
         yStops[1],
         memo + movementY
-      )
+      );
 
       set({
         y: newY,
         immediate: true
-      })
+      });
 
-      return memo
+      return memo;
     }
-  )
+  );
 
   return (
     <StyledNotificationContainer>
@@ -74,34 +74,40 @@ const Notification = ({ children, hideNotification }) => {
         {children}
       </StyledNotification>
     </StyledNotificationContainer>
-  )
-}
+  );
+};
 
 const NotificationDemo = () => {
-  const [notificationVisible, setNotificationVisible] = React.useState(false)
+  const [notificationVisible, setNotificationVisible] = React.useState(false);
 
   return (
     <StyledContainer>
       <button
         onClick={() => {
-          setNotificationVisible(prevShow => !prevShow)
+          setNotificationVisible(prevShow => !prevShow);
         }}
       >
-        👇 show notification
+        <span role="img" aria-label="show notification">
+          👇show notification
+        </span>
       </button>
       {notificationVisible && (
         <Notification
           visible={notificationVisible}
           hideNotification={() => {
-            setNotificationVisible(false)
+            setNotificationVisible(false);
           }}
         >
-          <div>🐶</div>
+          <div>
+            <span role="img" aria-label="dog">
+              🐶
+            </span>
+          </div>
           <div>&nbsp;&nbsp;just saying hi</div>
         </Notification>
       )}
     </StyledContainer>
-  )
-}
+  );
+};
 
-export default NotificationDemo
+export default NotificationDemo;
